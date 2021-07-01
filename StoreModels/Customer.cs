ebe14632b02817008e4a9017c2e7469385d26441
Suraj Kalika <54459765+surajkalika2@users.noreply.github.com>
2021-07-01 11:56:44 -0400

    Modified ToString method()

--- StoreModels/Customer.cs
+++ StoreModels/Customer.cs
@@ -17,7 +17,7 @@ namespace StoreModel{
 
         public override string ToString()
         {
-            return "Name: " + Name + Address +Email;
+            return $"Name:  + {Name}, Address:{Address} Email:{Email}";
         }
     }
 
