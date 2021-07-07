8962c0e964fe0efd67bac17b41b2505026acfa51
surajkalika2 <surajkalika2@gmail.com>
2021-07-07 14:51:51 -0400

    Generated Customer Entity

---
+++ StoreDL/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace StoreDL.Entities
+{
+    public partial class Customer
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+    }
+}
