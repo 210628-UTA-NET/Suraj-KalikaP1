362297d14a35bfa17a6cb3cd7241584f627bb013
surajkalika2 <surajkalika2@gmail.com>
2021-07-12 00:46:16 -0400

    Updated Models

--- StoreModels/Customer.cs
+++ StoreModels/Customer.cs
@@ -14,12 +14,12 @@ namespace StoreModel{
 
         public string Email{get;set;}
 
-        
+        public string PhoneNumber{get;set;}
         public List<Orders> Orders {get;set;}
 
         public override string ToString()
         {
-            return "ID: "+Id+"| Name: " + Name+"| Address: " + Address+"| Email: " +Email;
+            return "ID: "+Id+"| Name: " + Name+"| Address: " + Address+"| Email: " +Email+"| Phone Number"+ PhoneNumber;
         }
     }
 
