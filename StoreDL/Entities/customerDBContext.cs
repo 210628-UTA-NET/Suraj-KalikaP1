d145e88effbf10dcd4c5a0a82cc9ce46798fa572
surajkalika2 <surajkalika2@gmail.com>
2021-07-12 00:45:54 -0400

    remade DBContext

---
+++ StoreDL/Entities/customerDBContext.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace StoreDL.Entities
+{
+    public partial class customerDBContext : DbContext
+    {
+        public customerDBContext()
+        {
+        }
+
+        public customerDBContext(DbContextOptions<customerDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<LineItem> LineItems { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<StoreFront> StoreFronts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("Customer");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CustomerName)
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PhoneNumber)
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.OrdersNavigation)
+                    .WithMany(p => p.Customers)
+                    .HasForeignKey(d => d.Orders)
+                    .HasConstraintName("FK__Customer__Orders__03F0984C");
+            });
+
+            modelBuilder.Entity<LineItem>(entity =>
+            {
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.LineItems)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("FK__LineItems__Produ__7B5B524B");
+
+                entity.HasOne(d => d.Store)
+                    .WithMany(p => p.LineItems)
+                    .HasForeignKey(d => d.StoreId)
+                    .HasConstraintName("FK__LineItems__Store__7C4F7684");
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.HasOne(d => d.LineItem)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.LineItemId)
+                    .HasConstraintName("FK__Orders__LineItem__01142BA1");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Category)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(7, 2)");
+            });
+
+            modelBuilder.Entity<StoreFront>(entity =>
+            {
+                entity.ToTable("StoreFront");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StoreName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
